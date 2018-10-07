ca5176fe81aec6c58530c1c716433d3b0043683f
KirillSavvin <31733535+KirillSavvin@users.noreply.github.com>
2018-10-07 20:24:11 +0300

    Initial commit

---
+++ public_html/script.js
@@ -0,0 +1,53 @@
+var array = ['.first', '.second', '.third'];
+
+$('li.first').click(function () {
+	$('article').css('display','none');
+	$('article.first').css('display','block');
+	$('.tabs li').removeClass('new-style');
+	$('li.first').addClass('new-style');
+})
+
+$('li.second').click(function () {
+	$('article').css('display','none');
+	$('article.second').css('display','block');
+	$('.tabs li').removeClass('new-style');
+	$('li.second').addClass('new-style');
+})
+
+
+$('li.third').click(function () {
+	$('article').css('display','none');
+	$('article.third').css('display','block');
+	$('.tabs li').removeClass('new-style');
+	$('li.third').addClass('new-style');
+})
+
+
+$('li.fourth').click(function () {
+	$('article').css('display','none');
+	$('article.fourth').css('display','block');
+	$('.tabs li').removeClass('new-style');
+	$('li.fourth').addClass('new-style');
+})
+
+
+$('li.fifth').click(function () {
+	$('article').css('display','none');
+	$('article.fifth').css('display','block');
+	$('.tabs li').removeClass('new-style');
+	$('li.fifth').addClass('new-style');
+})
+
+$('li.sixth').click(function () {
+	$('article').css('display','none');
+	$('article.sixth').css('display','block');
+	$('.tabs li').removeClass('new-style');
+	$('li.sixth').addClass('new-style');
+})
+
+$('li.seventh').click(function () {
+	$('article').css('display','none');
+	$('article.seventh').css('display','block');
+	$('.tabs li').removeClass('new-style');
+	$('li.seventh').addClass('new-style');
+})
